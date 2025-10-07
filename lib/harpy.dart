fb0761d7060b8c396c48a8679308f2efe614f384
Sergey S. Moiseev <sergey@moses-team.ru>
2025-10-07 14:29:40 +0300

    chore: remove deprecated scheduler export and update dependencies

--- lib/harpy.dart
+++ lib/harpy.dart
@@ -27,9 +27,6 @@ export 'src/middleware/scheduler_middleware.dart';
 export 'src/routing/route.dart';
 // Routing
 export 'src/routing/router.dart';
-// Scheduler (deprecated - use SchedulerMiddleware via app.enableScheduler())
-@Deprecated('Use app.enableScheduler() and app.addTask() instead')
-export 'src/scheduler/scheduler.dart';
 // Task system
 export 'src/scheduler/task.dart';
 // Core server
